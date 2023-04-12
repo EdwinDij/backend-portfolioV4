b842b6c33ab5b20f11bc9d330775c2cae2acbe5f
EdwinDij <edwin.d899@gmail.com>
2023-04-12 16:18:26 +0200

    ajout du data.json avec les projets

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import HTTPException
 from email.mime.text import MIMEText
-from typing import Union
-from urllib import request
-from fastapi import FastAPI, Form
 from dotenv import load_dotenv
 
+import smtplib
 import os
 import smtplib
 
@@ -17,3 +18,31 @@ def read_root():
     return {"Hello": "World"}
 
 @app.post("/api/send-email")
+async def send_email(request: Request):
+    form = await request.form()
+    name = form.get('name')
+    email_sender = form.get('email')
+    message = form.get('message')
+    
+    if not name or not email_sender or not message:
+        raise HTTPException(status_code=400, detail='Champs manquants')
+    
+    
+    recipient = email
+    subject = f'Nouveau message de {name} depuis le portfolio!'
+    body = f'Nom: {name} \nEmail: {email_sender} \nMessage: {message}'
+    
+    message = MIMEText(body)
+    message['Subject'] = subject
+    message['From'] = email_sender
+    message['To'] = recipient
+    message['Importance'] = 'High'
+    
+    try: 
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+            smtp.login(email, password)
+            smtp.sendmail(email_sender, recipient, message.as_string())
+            return JSONResponse(status_code=200, content={'message': 'Message envoyé avec succès!'})
+    except smtplib.SMTPException as e:
+        raise HTTPException(status_code=500, detail='Erreur lors de l\'envoi du message: ' + str(e))
+
